55d133e771b2f988e84a6e200bfb00061ff33a57
patrickduhaime <32279890+patrickduhaime@users.noreply.github.com>
2018-04-19 08:19:48 -0400

    added connect string

--- src/conceptionldevoir3/ConceptionLDevoir3.java
+++ src/conceptionldevoir3/ConceptionLDevoir3.java
@@ -18,15 +18,24 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import ViewController.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  *
  * @author VIN
  */
 public class ConceptionLDevoir3 extends Application {
-    
+
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-      CreateFakeDataMembers();
+     // CreateFakeDataMembers();
       String fxmlResource = Constants.NAVPATH_FXML;      
       FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlResource));
       fxmlLoader.setController(new MainNavigateController());
@@ -44,15 +53,33 @@ public class ConceptionLDevoir3 extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+        int rndRue = new ThreadLocalRandom().current().nextInt(1,169);
+        int rndPhone  = new ThreadLocalRandom().current().nextInt(1212,9999);
+        int rndPermis  = new ThreadLocalRandom().current().nextInt(,9999);
+        try{
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection con;
+
+            con = DriverManager.getConnection(
+                            "jdbc:oracle:thin:@neptune.uqtr.ca:1521:coursbd","SMI1002_07","26nbkv28");
+
+            Statement stmt=con.createStatement();
+            stmt.executeQuery("insert into CLIENT (NOM, PRENOM, ADRESSE, NOTEL, NOPERMIS) VALUES (Client"+i+",Nom"+i+ ", " + rndRue + "ieme Avenue\, Montreal\, Quebec\, H2L-7B2, 514-555-" + rndPhone + " " + rndPermis);
+
+
+            con.close();
+
+        }catch(Exception e){ System.out.println(e);}
+
     }
     
-    private void CreateFakeDataMembers(){
-        MembreServices.getInstance().insertMember(new Membre("Thomas","Jeff","38 des la hate","JFF7865FR","Ste-Marthe","Quebec","JFk@asd.com","215-888-9895","Canada","H6RU4E"));
-      MembreServices.getInstance().insertMember(new Membre("Marie","Jeff","67 marion","Maria8865FR","Ste-Marthe","Quebec","JFk@asd.com","215-888-9895","Canada","H6RU4E"));
-      MembreServices.getInstance().insertMember(new Membre("Thomas","Jeff","38 des la hate","JFF7865FR","Ste-Marthe","Quebec","JFk@asd.com","215-888-9895","Canada","H6RU4E"));
-      MembreServices.getInstance().insertMember(new Membre("Annie","Martin","78 de l'amour","Annie8865FR","Ste-Marthe","Quebec","JFk@asd.com","215-888-9895","Canada","H6RU4E"));
-      MembreServices.getInstance().insertMember(new Membre("Marco","Biron","77 des la hate","MRC87346yy","Ste-Marthe","Quebec","JFk@asd.com","215-888-9895","Canada","H6RU4E"));
-      MembreServices.getInstance().insertMember(new Membre("Norma","Jones","44782 marionne","NOORMY65FR","Ste-Marthe","Quebec","JFk@asd.com","215-888-9895","Canada","H6RU4E"));
-    }
-    
+     private void CreateFakeDataMembers(){
+            MembreServices.getInstance().insertMember(new Membre("Thomas","Jeff","38 des la hate","JFF7865FR","Ste-Marthe","Quebec","JFk@asd.com","215-888-9895","Canada","H6RU4E"));
+          MembreServices.getInstance().insertMember(new Membre("Marie","Jeff","67 marion","Maria8865FR","Ste-Marthe","Quebec","JFk@asd.com","215-888-9895","Canada","H6RU4E"));
+          MembreServices.getInstance().insertMember(new Membre("Thomas","Jeff","38 des la hate","JFF7865FR","Ste-Marthe","Quebec","JFk@asd.com","215-888-9895","Canada","H6RU4E"));
+          MembreServices.getInstance().insertMember(new Membre("Annie","Martin","78 de l'amour","Annie8865FR","Ste-Marthe","Quebec","JFk@asd.com","215-888-9895","Canada","H6RU4E"));
+          MembreServices.getInstance().insertMember(new Membre("Marco","Biron","77 des la hate","MRC87346yy","Ste-Marthe","Quebec","JFk@asd.com","215-888-9895","Canada","H6RU4E"));
+          MembreServices.getInstance().insertMember(new Membre("Norma","Jones","44782 marionne","NOORMY65FR","Ste-Marthe","Quebec","JFk@asd.com","215-888-9895","Canada","H6RU4E"));
+        }
 }
